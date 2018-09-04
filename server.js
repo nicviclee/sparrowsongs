3b42c5d3fe88148b37025293971ba7b36208911a
nicviclee <nicviclee@hotmail.com>
2018-09-04 00:12:42 -0700

    Update app name for deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/sparrowsongs'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/sparrowsongs/index.html'));
+res.sendFile(path.join(__dirname+'/dist/sparrowsongs/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
